35e4bf39e4f180850bd2d24ced0bc21c08cb7bd2
Shakia Tasnim <99421796+Shakia123@users.noreply.github.com>
2024-06-12 21:11:55 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -56,37 +56,6 @@ include 'connection.php';
                 <li class="nav-item">
                   <a class="nav-link" href="#">Contact</a>
                 </li>
-
-                <!--<li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Staff
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Sign in</a></li>
-                    <li><a class="dropdown-item" href="#">Sign up</a></li>
-                  </ul>
-                </li>
-
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Member
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Sign in</a></li>
-                    <li><a class="dropdown-item" href="#">Sign up</a></li>
-                  </ul>
-                </li>
-
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Donor
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Sign in</a></li>
-                    <li><a class="dropdown-item" href="#">Sign up</a></li>
-                  </ul>
-                </li> -->
-                
               </ul>
             </div>
           </div>
